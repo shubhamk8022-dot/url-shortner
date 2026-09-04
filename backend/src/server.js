2631a4b87d0b49c0e008f1a7946f77fb23e84ad9
shubham kumar <shubhamk8022@gmail.com>
2026-09-04 16:46:00 +0530

    feat : added mysql connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,22 @@
 const app = require("./app");
+// const dbConnectionPool = require("./config/database.config.js");
+const dbConnectionPool = require("./config/db.config.js")
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+const startServer = async () => {
+    try {
+        await dbConnectionPool.query("SELECT 1");
+
+        console.log("MySQL database connected successfully");
+
+        app.listen(PORT, () => {
+            console.log(`Server running on http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.error("Unable to connect to MySQL:", error.message);
+        process.exit(1);
+    }
+};
+
+startServer();
